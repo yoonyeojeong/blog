42d21e1f5caa90df5a3fded473c0406ea06fb152
yeojoengyoon <uoi798@gmail.com>
2024-01-02 15:32:31 +0900

    과연

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> 과연 될까
         </p>
         <a
           className="App-link"
